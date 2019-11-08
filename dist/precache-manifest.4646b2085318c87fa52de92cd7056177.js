d084fe2ae205535bd40e47f70185e37fd8cc2301
blacktiny <blacktiny126@hotmail.com>
2019-11-08 05:33:33 -0800

    initial commit

---
+++ dist/precache-manifest.4646b2085318c87fa52de92cd7056177.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "522426fac71f43e6c6754732e4798ff8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "716f212d333f57c2e179",
+    "url": "/static/css/main.49be70bd.chunk.css"
+  },
+  {
+    "revision": "87d6c4a625cf5fd751f6",
+    "url": "/static/js/2.36664d3f.chunk.js"
+  },
+  {
+    "revision": "716f212d333f57c2e179",
+    "url": "/static/js/main.e1dc41db.chunk.js"
+  },
+  {
+    "revision": "59a0af3df2617f5ab145",
+    "url": "/static/js/runtime-main.959548da.js"
+  }
+]);
